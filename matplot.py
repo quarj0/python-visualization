eabd64b82eec1edb27ee49add0ad3e0d3be38fb9
hacks-and-codes <owusuansahkwadwo24@gmail.com>
2022-08-01 12:07:04 +0000

    another update

--- matplot.py
+++ matplot.py
@@ -1,13 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Generating 25 random numbers from 0 to 10
 x=np.linspace(0,10,25)
 y=x*x+2
+# plotting x and y values only
 # plt.plot(x,y)
 # plt.show()
 
-plt.subplot(1,2,1)
-plt.plot(x,y)
-plt.subplot(1,2,2)
-plt.plot(y,x)
-plt.show()
+# plotting the x and y values side by side
+# plt.subplot(1,2,1)
+# plt.plot(x,y)
+# plt.subplot(1,2,2)
+# plt.plot(y,x)
+# plt.show()
+
