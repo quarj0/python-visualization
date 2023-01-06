1516accd8d275f6ba8b16e39d0cac89aa9f6150c
Owusu Ansah Kwadwo <54241472+hacks-and-codes@users.noreply.github.com>
2023-01-06 05:44:35 +0000

    some data for pandas examples

--- December 16 2022/eg.py
+++ December 16 2022/eg.py
@@ -1,1 +1,8 @@
-import ipython
+import matplotlib.pyplot as plt
+%matplotlib inline
+import numpy as np
+
+x = np.linspace(0,10,100)
+fig  = plt.figure()
+plt.plot(x, np.sin(x), '-')
+plt.plot(x, np.cos(x), '--')
---
+++ December 16 2022/gdp.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+from IPython.display import display
+data = pd.read_csv("H:\python-visualization\Gh gdp sample.csv")
+df  = pd.DataFrame(data)
+display(df)
